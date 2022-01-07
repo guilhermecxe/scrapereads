7da8a5278d6e4e50b7914392cc907831beb1f5af
Guilherme Alves <guilhermecxe@hotmail.com>
2022-01-07 17:13:31 -0300

    Organizing imports so it is possible to import goodreads_scrape functions directly from scrapereads

--- scrapereads/__init__.py
+++ scrapereads/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Guilherme Alves"""
 __email__ = 'guilhermecxe@hotmail.com'
 __version__ = '0.1.0'
+
+from scrapereads.goodreads_scrape import (
+    get_book_infos,
+    get_choice_awards_categories,
+    get_choice_awards_nominees
+)
---
+++ scrapereads/goodreads_scrape/__init__.py
@@ -0,0 +1,5 @@
+from scrapereads.goodreads_scrape.goodreads_scrape import (
+    get_book_infos,
+    get_choice_awards_categories,
+    get_choice_awards_nominees
+)
